ea1ce6314f733b63bced02421bf23eec69fe9563
EneaSema <sema.enea1993@gmail.com>
2025-11-18 18:30:49 +0100

    create calculator basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
+// IMPORT
+
 import Button from "./components/Button";
 import { useState } from "react";
 import { calculate } from "./utils/calculate.js";
 
 export default function App() {
+  // STATI
   const [startValue, setStartValue] = useState(`0`);
   const [firstValue, setFirstValue] = useState(null);
   const [selectOperator, setSelectOperator] = useState(null);
   const [waitingSecondValue, setWaitingSecondValue] = useState(false);
 
+  // Array con le Variabili
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   const operators = [`+`, `-`, `*`, `/`];
+  //   Funzioni handle per gestire  le varie dinamiche:
 
+  //  gestione con i numeri:
   const handleNumberClick = (number) => {
+    //  controllo del numero passato
     if (number === `.` && startValue.includes(`.`)) {
       return;
     }
+    // controllo se l'attesa del secondo numero è vera,
+    //  in questo caso assegno vado a settare il valore inziale con 0. oppure il numero in base al valore del numero,
+    // poi  setto a falso lo stato di attesa
     if (waitingSecondValue) {
       setStartValue(number === `.` ? `0.` : number);
       setWaitingSecondValue(false);
@@ -24,14 +34,14 @@ export default function App() {
       );
     }
   };
-
+  //   funzione di pulizia della calcolatrice, azzera tutto
   const handleClearClick = () => {
     setStartValue(`0`);
     setFirstValue(null);
     setSelectOperator(null);
     setWaitingSecondValue(false);
   };
-
+  //  funzione in merito al salvataggio del primo numero
   const handleOperatorClick = (nextOperator) => {
     const inputValue = parseFloat(startValue);
     if (firstValue === null) {
@@ -51,6 +61,7 @@ export default function App() {
     setWaitingSecondValue(true);
     setSelectOperator(nextOperator);
   };
+  //  funzione per l'esecuzione del calcolo
   const handleEqualsClick = () => {
     if (selectOperator === null || firstValue === null) {
       return;
@@ -68,7 +79,7 @@ export default function App() {
     setSelectOperator(null);
     setWaitingSecondValue(false);
   };
-
+  //  centrallizoo le precedenti funzioni qui, in base al valore del bottone cliccato
   const handleButtonClick = (value) => {
     const val = String(value);
 
--- src/utils/calculate.js
+++ src/utils/calculate.js
@@ -1,16 +1,18 @@
+//  funzione calcolatrice 4 operazioni base
+
 export const calculate = (num1, operator, num2) => {
   switch (operator) {
     case `+`:
       return num1 + num2;
 
     case `-`:
-      return num1 > num2 ? num1 - num2 : console.log("Errore");
+      return num1 > num2 ? num1 - num2 : "Errore";
 
     case `*`:
       return num1 * num2;
 
     case `/`:
-      return num2 === 0 ? console.log(Errore) : num1 / num2;
+      return num2 === 0 ? "Errore" : num1 / num2;
 
     default:
       return num2;
